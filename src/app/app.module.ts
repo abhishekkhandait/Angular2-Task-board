40e85ada61e32bda2dc5d76bdc43938ff927cddf
Abhishek <khandaitabhishek@gmail.com>
2017-04-27 21:24:34 +0530

    added imports for datepicker and addtaskcomponent to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DndModule } from 'ng2-dnd'; 
-
+import { DatepickerModule } from 'angular2-material-datepicker';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './core/nav/nav.component';
 import { BoardComponent } from './board/board.component';
 import { TaskCardComponent } from './board/shared/task-card/task-card.component';
+import { AddTaskComponent } from './board/shared/add-task/add-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     BoardComponent,
-    TaskCardComponent
+    TaskCardComponent,
+    AddTaskComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,9 +28,10 @@ import { TaskCardComponent } from './board/shared/task-card/task-card.component'
     MaterialModule.forRoot(),
     BrowserAnimationsModule,
     DndModule.forRoot(),
+    DatepickerModule,
   ],
   exports: [DndModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AddTaskComponent]
 })
 export class AppModule { }
