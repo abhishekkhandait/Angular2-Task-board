0acedb6f4bc7069dcdffe94cdfcd3efc34a24a54
Abhishek <khandaitabhishek@gmail.com>
2017-04-27 21:23:37 +0530

    created task component

---
+++ src/app/board/shared/add-task/add-task.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MdDialog, MdDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+
+  constructor(public dialogRef: MdDialogRef<AddTaskComponent>) { }
+
+  ngOnInit() {
+  }
+
+}
