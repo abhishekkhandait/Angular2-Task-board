9e5bbd3c9400887539f4f901f44bd6bc3b4ea0c0
Abhishek <khandaitabhishek@gmail.com>
2017-04-27 21:26:52 +0530

    added sort menu; added new task dialog;

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AddTaskComponent } from '../board/shared/add-task/add-task.component';
 import { TasksService } from '../services/tasks.service';
 import { BoardModel } from '../models/board-model';
 import { TaskModel } from '../models/task-model';
+import {MdDialog, MdDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-board',
@@ -17,27 +19,29 @@ export class BoardComponent implements OnInit {
   @Input() boardid: string;
 
   constructor(
-    private tasksService: TasksService
+    private tasksService: TasksService,
+    public dialog: MdDialog,
   ) { }
 
   ngOnInit() {
     this.getBoardInfo();
     this.getTasks();
+    this.SetSortOrder('priority');
   }
 
   tiles = [
-    { text: 'New', cols: 1, rows: 1.8, color: '#222' },
-    { text: 'On-Hold', cols: 1, rows: 1.8, color: '#222' },
-    { text: 'In-Progress', cols: 1, rows: 1.8, color: '#222' },
-    { text: 'Done', cols: 1, rows: 1.8, color: '#222' },
+    { text: 'New', cols: 1, rows: 1.75, color: '#CFD8DC', titlecolor: '#03A9F4' },
+    { text: 'On-Hold', cols: 1, rows: 1.75, color: '#CFD8DC', titlecolor: '#f5c942' },
+    { text: 'In-Progress', cols: 1, rows: 1.75, color: '#CFD8DC', titlecolor: '#d066e2' },
+    { text: 'Done', cols: 1, rows: 1.75, color: '#CFD8DC', titlecolor: '#ff6b68' },
   ];
+
+  sortOrder: string;
 
   simpleDrop($event: any, tileText: string) {
     if($event.dragData.list !== tileText){
-      this.UpdateTask($event.dragData.boardId, $event.dragData.list, tileText);  
+      this.UpdateTask($event.dragData.id, tileText);  
     }
-    console.log($event.dragData);
-    console.log(tileText);
   }
 
   getBoardInfo() {
@@ -58,13 +62,23 @@ export class BoardComponent implements OnInit {
       });
   }
 
-  UpdateTask(id, sList, tList){
-    console.log('PatchStart');
-    this.tasksService.updateTask(id, sList, tList)
+  UpdateTask(id, tList){
+    this.tasksService.updateTask(id, tList)
     .subscribe(
       updatedTask => this.updatedTask =  updatedTask,
       err => {
         console.log(err);
       });
   }
+
+  SetSortOrder(sortBy){
+    this.sortOrder = sortBy;
+  }
+
+  addTask(){
+    let newTaskBox = this.dialog.open(AddTaskComponent);
+    newTaskBox.afterClosed().subscribe(result => {
+      this.boardid = result;
+    });
+    }
 }
