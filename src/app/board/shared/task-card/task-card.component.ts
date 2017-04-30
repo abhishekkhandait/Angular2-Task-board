36e1969ed23da42cdd3ae7a96e583676bb240bbb
Abhishek Khandait <khandaitabhishek@gmail.com>
2017-04-30 18:07:55 +0530

    added animations for tiles

--- src/app/board/shared/task-card/task-card.component.ts
+++ src/app/board/shared/task-card/task-card.component.ts
@@ -1,9 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, animate, keyframes, style, transition } from '@angular/core';
 
 @Component({
   selector: 'app-board-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.css']
+  styleUrls: ['./task-card.component.css'],
+  animations: [
+  trigger('flyInOut', [
+    state('in', style({transform: 'translateY(0)'})),
+    transition('void => *', [
+      animate(700, keyframes([
+        style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+        style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+      ]))
+    ])
+  ])
+]
 })
 export class TaskCardComponent implements OnInit {
   @Input() task;
