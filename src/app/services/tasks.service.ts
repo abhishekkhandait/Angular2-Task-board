e58c6f2c12b5baac19fdf6c74382fec21c38057f
Abhishek Khandait <khandaitabhishek@gmail.com>
2017-05-01 19:18:33 +0530

    removed tslint warrnings modified getboard() to getboards(0

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -9,45 +9,43 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class TasksService {
-  // Resolve HTTP using the constructor
-  constructor(private http: Http) { }
   // private instance variable to hold base url
   private baseUrl = 'http://localhost:3001/';
   private boardUrl = 'http://localhost:3001/boards/';
   private taskUrl = 'http://localhost:3001/tasks/';
 
-  getBoard(boardid: string): Observable<BoardModel> {
-    let params: URLSearchParams = new URLSearchParams();
-    params.set('id', boardid);
+  // Resolve HTTP using the constructor
+  constructor(private http: Http) { }
 
-    return this.http.get(this.boardUrl, { search: params })
+  getBoards(): Observable<BoardModel[]> {
+    return this.http.get(this.boardUrl)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error) || 'Server Error');
   }
 
   getTasks(boardid: string): Observable<TaskModel[]> {
-    var url = this.boardUrl + boardid + '/tasks';
+    const url = this.boardUrl + boardid + '/tasks';
     return this.http.get(url)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error) || 'Server Error');
   }
 
   updateTask(id: string, targetlist: string): Observable<TaskModel> {
-    var url = this.taskUrl + id;
+    const url = this.taskUrl + id;
     console.log(url);
-    return this.http.patch(url, { "list": targetlist })
+    return this.http.patch(url, { 'list': targetlist })
       .map((res: Response) => res.json)
       .catch((error: any) => Observable.throw(error.json().error) || 'Server Error');
   }
 
   createTask(boardid:string, list: string, name: string, dueDate: string, priority: string): Observable<TaskModel>{
     console.log('post');
-    var url = this.boardUrl + boardid + '/tasks';
-    var taskobj = {
-      "list": list,
-      "name": name,
-      "dueDate": dueDate,
-      "priority": priority
+    const url = this.boardUrl + boardid + '/tasks';
+    const taskobj = {
+      'list': list,
+      'name': name,
+      'dueDate': dueDate,
+      'priority': priority
     }
     console.log(taskobj);
 
